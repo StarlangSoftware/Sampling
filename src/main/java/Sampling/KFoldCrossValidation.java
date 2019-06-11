32e11cca91a1aaeeedce5293e52bdc7229f0287d
tugcegns <tugce.gns.94@gmail.com>
2019-06-11 15:41:08 +0300

    k fold cross validation

--- src/main/java/Sampling/KFoldCrossValidation.java
+++ src/main/java/Sampling/KFoldCrossValidation.java
@@ -9,6 +9,13 @@ public class KFoldCrossValidation<T> extends CrossValidation<T>{
     private List<T> instanceList;
     private int N;
 
+    /**
+     * Constructor of KFoldCrossValidation class which takes a list of instanceList, K and seed number as inputs.
+     * It initializes instanceList, N as size of instanceList and K, also shuffles the given instanceList randomly.
+     * @param instanceList data given
+     * @param K fold number
+     * @param seed integer input for random generator
+     */
     public KFoldCrossValidation(List<T> instanceList, int K, int seed){
         this.instanceList = instanceList;
         Collections.shuffle(instanceList, new Random(seed));
@@ -16,6 +23,11 @@ public class KFoldCrossValidation<T> extends CrossValidation<T>{
         this.K = K;
     }
 
+    /**
+     * This method returns the train data of the given instanceList.
+     * @param k number of groups that a given data is to be split into
+     * @return arrayList<T> trainFold
+     */
     public ArrayList<T> getTrainFold(int k){
         ArrayList<T> trainFold = new ArrayList<T>();
         for (int i = 0; i < (k * N) / K; i++){
@@ -27,6 +39,11 @@ public class KFoldCrossValidation<T> extends CrossValidation<T>{
         return trainFold;
     }
 
+    /**
+     * This method returns the test data of the given instanceList.
+     * @param k number of groups that a given data is to be split into
+     * @return arrayList<T> testFold
+     */
     public ArrayList<T> getTestFold(int k){
         ArrayList<T> testFold = new ArrayList<T>();
         for (int i = (k * N) / K; i < ((k + 1) * N) / K; i++){
